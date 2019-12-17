178d3b74c4288f56bd22e877e6e1770b89677b78
Mike Zak <feanorr@gmail.com>
2019-12-17 15:09:28 +0200

    [NOD-495] Deleted everything except faucet

--- log.go
+++ log.go
@@ -1,8 +1,3 @@
-// Copyright (c) 2013-2017 The btcsuite developers
-// Copyright (c) 2017 The Decred developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
@@ -10,6 +5,7 @@ import (
 	"github.com/kaspanet/kaspad/util/panics"
 )
 
-var kasdLog, _ = logger.Get(logger.SubsystemTags.KASD)
-var spawn = panics.GoroutineWrapperFunc(kasdLog)
-var srvrLog, _ = logger.Get(logger.SubsystemTags.SRVR)
+var (
+	log   = logger.BackendLog.Logger("FAUC")
+	spawn = panics.GoroutineWrapperFunc(log)
+)
